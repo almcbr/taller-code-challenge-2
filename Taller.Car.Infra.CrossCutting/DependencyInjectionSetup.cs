aedc01d034c600341771c601a47663ef1e75ac37
Andre Luiz de Castro <andreluiz.castro@gmail.com>
2023-02-21 17:43:22 -0300

    Adding CrossCutting Layer

---
+++ Taller.Car.Infra.CrossCutting/DependencyInjectionSetup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Taller.Car.Domain.Interfaces;
+using Taller.Car.Domain.Services;
+using Taller.Car.Infra.Data;
+using Taller.Car.Infra.Data.Repository;
+
+namespace Taller.Car.Infra.CrossCutting;
+
+public static class DependencyInjectionSetup
+{
+    public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+    {
+        services.AddDbContext<TallerDBContext>();
+        services.AddScoped<ICarService, CarService>();
+        services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+
+        return services;
+    }
+}
--- src/Taller.Car.API/Program.cs
+++ src/Taller.Car.API/Program.cs
@@ -1,18 +1,10 @@
-using Taller.Car.Domain.Interfaces;
-using Taller.Car.Domain.Services;
-using Taller.Car.Infra.Data;
-using Taller.Car.Infra.Data.Repository;
+using Taller.Car.Infra.CrossCutting;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddDbContext<TallerDBContext>();
-
-//Dependency Injection Config
-builder.Services.AddScoped<ICarService,CarService>();
-builder.Services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
-
+builder.Services.ResolveDependencies();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
